50ecdbba454ac7f35b16961513ff8a069bee7d1e
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-06-28 21:04:43 +0200

    feat: move from tag to js file & introduce local comp

---
+++ init.js
@@ -0,0 +1,26 @@
+let ButtonComponent = {
+  template: `<button style="background-color: red">{{name}}</button>`,
+  props: {
+    name: String,
+  },
+};
+
+// Global version
+// Vue.component("utton", {
+//   template: `<button style="background-color: red">BTN</button>`,
+// });
+
+new Vue({
+  el: "#app",
+  components: {
+    "n-button": ButtonComponent,
+  },
+  data: {
+    message: "Hello Vue!",
+  },
+  methods: {
+    replaceText: function () {
+      this.message = "lol";
+    },
+  },
+});

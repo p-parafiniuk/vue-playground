79ad53ca399a5b085e9160e3006f928b7cf04489
p-parafiniuk <pawel.parafiniuk@netguru.com>
2021-06-28 22:03:22 +0200

    feat: Introduce better inline styling for test button component

--- v2-simplified/init.js
+++ v2-simplified/init.js
@@ -1,7 +1,27 @@
 let ButtonComponent = {
-  template: `<button style="background-color: red">
+  template: `
+<button style="  display: inline-flex;
+  font-size: 0.875rem;
+  font-weight: 400;
+  justify-content: center;
+  align-items: center;
+  min-width: 75px;
+  padding: 0 32px;
+  border-radius: 3px;
+  border: 1px solid #0067e9;
+  letter-spacing: 0;
+  transition: all 0.3s ease-out;
+  line-height: 24px;
+  height: 36px;
+
+  cursor: pointer;
+
+  color: #ffffff;
+  background-color: #0067e9;
+">
       <slot></slot>
-</button>`,
+</button>
+`,
   props: {
     name: String,
   },
